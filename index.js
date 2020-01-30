565833706182a507de48d2f63e3ca8a0d7185dfc
Justin Lee <justinlee0022@gmail.com>
2020-01-30 08:39:27 -0800

    Improved readability

--- index.js
+++ index.js
@@ -22,43 +22,37 @@ const getNestedReplies = (replyObj, topicId) => {
 }
 
 const getDiscussions = async courseId => {
-  // get discussion topic ids for given course
   const discussionTopicIds = await getDiscussionTopicIds(courseId)
-  return Promise.all(
+  const discussionAndTopic = await Promise.all(
     discussionTopicIds
-      // concurrently retrieve discussion topic and any replies for a given discussion topic Id
       .map(topicId => Promise.all([
         capi.getFullDiscussion(courseId, topicId),
         capi.getDiscussionTopic(courseId, topicId)
-      ])
-        // array destructure result of the two API calls
-        .then(([discussion, topic]) => {
-          const topicTitle = topic.title
-          const topicMessage = topic.message
-          const author = topic.author
-          const timestamp = topic.created_at
-          const topicId = topic.id
-          // if discussion 'view' array has any element, then there are replies to this discussion
-          const replies = discussion.view.length > 0
-            ? discussion.view
-              // remove deleted posts as they contain no data
-              .filter(x => !x.deleted)
-              .map(reply => getNestedReplies(reply, topicId))
-            // discussion doesn't have 'view', so return empty array
-            : []
-          return {
-            topicTitle,
-            topicMessage,
-            id: topicId,
-            authorId: author.id || '',
-            timestamp,
-            replies
-          }
-        }))
+      ]))
   )
+  return discussionAndTopic.map(([discussion, topic]) => {
+    const topicTitle = topic.title
+    const topicMessage = topic.message
+    const author = topic.author
+    const timestamp = topic.created_at
+    const topicId = topic.id
+    const replies = discussion.view.length > 0
+      ? discussion.view
+        .filter(x => !x.deleted)
+        .map(reply => getNestedReplies(reply, topicId))
+      : []
+    return {
+      topicTitle,
+      topicMessage,
+      id: topicId,
+      authorId: author.id || '',
+      timestamp,
+      replies
+    }
+  })
 }
 
 // add course ID here!
-getDiscussions(46341)
+getDiscussions()
   // write result to CSV
   .then(discussions => writeToCSV(discussions))
--- util.js
+++ util.js
@@ -4,6 +4,38 @@ const flatten = arr => arr.reduce((acc, cur) =>
     : [...acc, cur]
 , [])
 
+const flattenTopicAndReplies = discussions => {
+  return discussions.reduce((acc, cur) => {
+    const timestamp = cur.timestamp
+    const authorId = cur.authorId
+    const discussionId = cur.id
+    const topicTitle = cur.topicTitle
+    const topicMessage = cur.topicMessage
+
+    acc.push({
+      type: 'topic',
+      timestamp,
+      authorId,
+      discussionId,
+      topicTitle,
+      topicMessage
+    })
+
+    flatten(cur.replies).forEach(reply => {
+      acc.push({
+        type: 'reply',
+        timestamp: reply.timestamp,
+        parentId: reply.parentId,
+        authorId: reply.authorId,
+        message: reply.message
+      })
+    })
+
+    return acc
+  }, [])
+}
+
 module.exports = {
-  flatten
+  flatten,
+  flattenTopicAndReplies
 }
