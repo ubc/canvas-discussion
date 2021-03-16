1b68abc96387fa310a1c65f6a2a1c5a9293cd8dd
Justin Lee <justin.lee@ubc.ca>
2021-03-16 15:11:01 -0700

    WIP

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const writeToCSV = require('./writeToCSV')
 const getDiscussionTopicIds = courseId => capi.getDiscussionTopics(courseId)
   .then(discussions => discussions.map(x => x.id))
 
+const getGroupDiscussionTopicIds = groupId => capi.getGroupDiscussionTopics(groupId)
+  .then(discussions => discussions.map(x => x.id))
+
 // recursively get nested replies and flatten result
 const getNestedReplies = (replyObj, participants, topicId) => {
   const replies = replyObj.hasOwnProperty('replies')
@@ -59,9 +62,46 @@ const getDiscussions = async courseId => {
   })
 }
 
+const getGroupDiscussions = async groupId => {
+  const discussionTopicIds = await getGroupDiscussionTopicIds(groupId)
+  const discussionAndTopic = await Promise.all(
+    discussionTopicIds
+      .map(topicId => Promise.all([
+        capi.getFullGroupDiscussion(groupId, topicId),
+        capi.getGroupDiscussionTopics(groupId, topicId)
+      ]))
+  )
+  return discussionAndTopic.map(([discussion, topic]) => {
+    const topicTitle = topic.title
+    const topicMessage = topic.message
+    const author = topic.author
+    const timestamp = topic.created_at
+    const topicId = topic.id
+    const participants = discussion.participants
+    const replies = discussion.view.length > 0
+      ? discussion.view
+        .filter(x => !x.deleted)
+        .map(reply => getNestedReplies(reply, participants, topicId))
+      : []
+    return {
+      topicTitle,
+      topicMessage,
+      id: topicId,
+      authorId: author.id || '',
+      authorName: author.display_name || '',
+      timestamp,
+      replies
+    }
+  })
+}
 
-Promise.all([
-  //{course id} add course ID here!
-].map(courseId => getDiscussions(courseId)
-  .then(discussions => writeToCSV(courseId, discussions))
-))
+// Promise.all([
+//   59748
+// ].map(courseId => getDiscussions(courseId)
+//   .then(discussions => writeToCSV(courseId, discussions))
+// ))
+
+capi.getGroupsInCourse(59748)
+  .then(groups => groups.map(group => group.id))
+  .then(groupIds => groupIds.map(groupId => getGroupDiscussions(groupId).then(x => console.log(x))))
+  // .then(x => console.log(x))
