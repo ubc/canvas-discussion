8026de49ce1815ae6871e8b96f7561807efedff7
alisonmyers <alison.myers@ubc.ca>
2024-05-22 10:50:11 -0700

    testing

--- summaryStats.js
+++ summaryStats.js
@@ -1,4 +1,15 @@
 const {getDiscussions, courseIds} = require('./index')
 
 const discussionData = courseIds.map(courseId => getDiscussions(courseId))
-//console.log(discussionData)
+console.log(discussionData)
+
+
+Promise.all(
+    courseIds.map(courseId =>
+      getDiscussions(courseId)
+        .then(discussion => console.log(discussion))
+    )
+  ).catch(error => {
+    const detailedErrorMessage = error.message || 'An unexpected error occurred';
+    console.error('Error processing discussions:', detailedErrorMessage);
+  });
