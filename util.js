750b8c658b9c323d4367111500c3a7770c7b4eeb
alisonmyers <alison.myers@ubc.ca>
2024-09-16 17:48:57 -0700

    standardize date functionality

--- util.js
+++ util.js
@@ -3,6 +3,14 @@ const path = require('path')
 const { DateTime } = require('luxon')
 const natural = require('natural')
 
+const escapeComment = comment => comment ? '"' + comment.replace(/"/g, "'") + '"' : ''
+
+const stripHTML = comment => comment ? comment.replace(/(<([^>]+)>)/gi, "").replace(/&nbsp;/g, " ") : ''
+
+const writeHeader = (pathToFile, headers) => fs.writeFileSync(pathToFile, headers + '\r\n')
+
+const appendRow = (pathToFile, row) => fs.appendFileSync(pathToFile, row.join(',') + '\r\n')
+
 const toPacificTimeString = (date) => 
   date
     ? DateTime.fromJSDate(date, { zone: 'utc' })   // Convert JS Date to Luxon DateTime in UTC
@@ -29,11 +37,6 @@ const median = (arr) => {
   return sorted.length % 2 !== 0 ? sorted[mid] : (sorted[mid - 1] + sorted[mid]) / 2
 }
 
-const escapeComment = comment => comment ? '"' + comment.replace(/"/g, "'") + '"' : ''
-const stripHTML = comment => comment ? comment.replace(/(<([^>]+)>)/gi, "").replace(/&nbsp;/g, " ") : ''
-const writeHeader = (pathToFile, headers) => fs.writeFileSync(pathToFile, headers + '\r\n')
-const appendRow = (pathToFile, row) => fs.appendFileSync(pathToFile, row.join(',') + '\r\n')
-
 // Word count function
 const getWordCount = (str) => {
   tokenizer = new natural.WordTokenizer()
@@ -43,6 +46,28 @@ const getWordCount = (str) => {
   const tokens = tokenizer.tokenize(cleanStr)
   const tokenCount = tokens.length 
   return tokenCount
+}
+
+const msToHours = ms => ms / (1000 * 60 * 60)
+const msToDays = ms => ms / (1000 * 60 * 60 * 24)
+
+// Function to calculate average time difference in a specified unit
+const calculateAverageTimeDiff = (posts, referenceTimestamp, timeUnit = 'hours') => {
+  const getTimeDiffInMs = (timestamp, reference) => {
+    const diff = timestamp - reference;
+    return diff > 0 ? diff : null;
+  }
+  
+  // Convert the average time difference to the specified unit
+  const unitConverter = timeUnit === 'hours' ? msToHours : (timeUnit === 'days' ? msToDays : msToHours)
+  
+  return unitConverter(
+    average(
+      posts
+        .map(post => getTimeDiffInMs(post.postTimestamp, referenceTimestamp))
+        .filter(diff => diff !== null)
+    )
+  )
 }
 
 // Function to calculate the topic summary
@@ -61,48 +86,28 @@ const postStatistics = (posts, referenceTimestamp) => {
     }
   }
 
-  // Word counts
   const wordCounts = posts.map(post => getWordCount(post.postMessage))
-
   const medianWordCount = Math.round(median(wordCounts) * 10) / 10
-
-  // Average time in hours from topicCreatedAt to postTimestamp
-  const timeDiffs = posts.map(post => {
-    return (post.postTimestamp - referenceTimestamp) 
-  })
-
-  const averageTimeDiff = parseFloat(average(timeDiffs).toFixed(1)) / (1000 * 60 * 60) // Convert from milliseconds to hours
 
   const postCountsByAuthor = posts.reduce((acc, post) => {
     acc[post.postAuthorId] = (acc[post.postAuthorId] || 0) + 1
     return acc
   }, {})
-  // Average number of posts per postAuthorId
 
   const postCounts = Object.values(postCountsByAuthor)
   const averagePostsPerAuthor = parseFloat(average(postCounts).toFixed(1))
-  
-  const firstReplyTimestamp = new Date(Math.min(...posts.map(
-    post => new Date(post.postTimestamp))))
 
-  const timeDiffsFromFirst = posts
-    .map(post => {
-      return post.postTimestamp > firstReplyTimestamp 
-        ? (post.postTimestamp - firstReplyTimestamp) / (1000 * 60 * 60) // Convert from milliseconds to hours
-        : null
-    })
-    .filter(diff => diff !== null)
-  
-  const averageTimeToPostFromFirst = timeDiffsFromFirst.length > 0
-    ? parseFloat(average(timeDiffsFromFirst).toFixed(1))
-    : 0
+  const averageTimeDiffFromReferenceInHours = calculateAverageTimeDiff(posts, referenceTimestamp, 'hours')
+  const averageTimeDiffFromFirstInHours = calculateAverageTimeDiff(posts, firstReplyTimestamp, 'hours')
+
     
   return {
     numberOfPosts,
     medianWordCount,
-    averageTimeDiff,
+    referenceTimestamp,
     firstReplyTimestamp,
-    averageTimeToPostFromFirst,
+    averageTimeDiffFromReferenceInHours,
+    averageTimeDiffFromFirstInHours,
     averagePostsPerAuthor
   }
 
